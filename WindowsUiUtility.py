794845b64dc5682bf4ae07fd70863101029b964d
Mike Thomas <dimike.thomas@icloud.com>
2020-07-11 14:49:18 +0200

    Add X/Y coordinate offset to click position according to position of window on screen to allow moving of BlueStacks application

--- WindowsUiUtility.py
+++ WindowsUiUtility.py
@@ -7,8 +7,10 @@ import numpy as np
 from ctypes import windll
 from PIL import ImageGrab
 
-windowsList = []
-topList = []
+_windowsList = []
+_topList = []
+_windowXCoordinateOffset = 0
+_windowYCoordinateOffset = 0
 
 # Make program aware of DPI scaling
 user32 = windll.user32
@@ -17,28 +19,37 @@ user32.SetProcessDPIAware()
 
 def enum_win(hwnd, result):
     win_text = win32gui.GetWindowText(hwnd)
-    windowsList.append((hwnd, win_text))
+    _windowsList.append((hwnd, win_text))
 
 
 def GetWindowHandleByWindowTitle(searchedWindowTitle):
-    win32gui.EnumWindows(enum_win, topList)
-    for (hwnd, win_text) in windowsList:
+    win32gui.EnumWindows(enum_win, _topList)
+    for (hwnd, win_text) in _windowsList:
         if searchedWindowTitle in win_text:
             return hwnd
 
 
 def GetScreenShotFromWindow(windowHwnd):
     position = win32gui.GetWindowRect(windowHwnd)
+    __setWindowCoordinateOffset(position)
     # Take screenshot
     screenshot = ImageGrab.grab(position)
     screenshot = np.array(screenshot)
     return cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
 
 
+def __setWindowCoordinateOffset(windowPosition):
+    global _windowXCoordinateOffset
+    _windowXCoordinateOffset = windowPosition[0]
+    global _windowYCoordinateOffset
+    _windowYCoordinateOffset = windowPosition[1]
+
+
 def SetCursorPosition(x, y):
     try:
-        win32api.SetCursorPos((x, y))
-    except:
+        win32api.SetCursorPos((x + _windowXCoordinateOffset, y + _windowYCoordinateOffset))
+    except Exception as e:
+        print(e.__doc__)
         print("Error in SetCursorPosition(", x, ", ", y, ")")
 
 
